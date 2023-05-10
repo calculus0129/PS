a56df35e2c5561cd8efe22a83e67cd95e5d0f079
calculus0129 <30453652+calculus0129@users.noreply.github.com>
2023-05-10 21:36:48 +0900

    solve b1717

---
+++ BOJ/C++/b1717/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myset {
+    vector<int> *s;
+public:
+    myset(vector<int> *p) : s(p) {}
+    myset& operator+=(myset &other); // for unionizing.
+    bool operator==(myset const &other) const;
+};
+
+myset& myset::operator+=(myset &other) {
+    if(this==&other) return *this;
+    s->insert(s->end(), other.s->begin(), other.s->end());
+    delete other.s;
+    other.s = s;
+    return *this;
+}
+
+bool myset::operator==(myset const &other) const {
+    return s == other.s;
+}
+
+int main()
+{
+    cin.tie(NULL); ios_base::sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    vector<myset> vec;
+    for(int i=0;i<=n;++i) {
+        vec.push_back(myset(new vector<int>(1, i)));
+    }
+    enum input {
+        UNION,  // 0
+        EQUAL,  // 1
+    };
+    int f, a, b;
+    while(m--) {
+        cin >> f >> a >> b;
+        switch(f) {
+        case UNION:
+            vec[a]+=vec[b];
+            break;
+        case EQUAL:
+            puts(vec[a]==vec[b]?"YES":"NO");
+        }
+    }
+    return 0;
+}
