74917916c8ac019b5255d5ec4bcfb5de3e116d28
calculus0129 <30453652+calculus0129@users.noreply.github.com>
2023-07-16 05:51:27 +0900

    solve b4589

---
+++ BOJ/Java/b4589_gnome-sequencing/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Gnomes:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(--n>=0) {
+            work(sc);
+        }
+    }
+    static void work(Scanner sc) {
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        if(x1<x2 && x2<x3 || x1>x2 && x2>x3) {
+            System.out.println("Ordered");
+        } else {
+            System.out.println("Unordered");
+        }
+    }
+}
