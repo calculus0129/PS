d5691b9d4163e3db6bad8200257f7fbf8ff87ea1
calculus0129 <30453652+calculus0129@users.noreply.github.com>
2023-08-20 00:27:29 +0900

    BufferedIO solve b1076

--- BOJ/Java/b1076/src/Main.java
+++ BOJ/Java/b1076/src/Main.java
@@ -1,9 +1,14 @@
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
-        String s1 = sc.next(), s2 = sc.next(), s3 = sc.next();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        String s1 = br.readLine(), s2 = br.readLine(), s3 = br.readLine();
         int a = 0;
         switch (s1) {
             case "black":
@@ -103,7 +108,10 @@ public class Main {
                 a = 9;
                 break;
         }
-        System.out.print(b);
-        while(b!=0 && a-->0) System.out.print("0");
+        String s = String.format("%d", b);
+        bw.write(s);
+        while(b!=0 && a-->0) bw.write("0");
+        bw.flush();
+        bw.close();
     }
 }
