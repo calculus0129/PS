2b77fe76ac5e6f94778ee622a132194a069aa34f
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-03-20 01:17:25 +0900

    solve b15700

---
+++ BOJ/C++/b15700.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    size_t m, n; cin >> m >> n;
+    cout << (m * n >> 1) ;
+    return 0;
+}
