4540d74a130c00bb4afc7a405d9a28877829c341
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-03-21 00:36:19 +0900

    solve b6887

---
+++ BOJ/C++/b6887.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    int n; cin >> n;
+    int s = sqrt(n);
+    cout << "The largest square has side length " << s << '.';
+    return 0;
+}
