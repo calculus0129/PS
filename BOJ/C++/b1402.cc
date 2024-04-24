94316c4e721ddf0448270d9815488e5855fb9d34
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-04-24 14:13:20 +0900

    solve b1402

---
+++ BOJ/C++/b1402.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    int t, a, b; cin >> t;
+    while(t--) {
+        cin >> a >> b;
+        cout << "yes\n";
+    }
+    return 0;
+}
