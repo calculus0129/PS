585b1253ab999390554a6a83a266c4d12238ae95
calculus0129 <ryanbhang129@naver.com>
2024-08-12 21:07:16 +0900

    try solving b1311

---
+++ BOJ/C++/b1311.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define BIG 200000
+using namespace std;
+int dp[1<<20], cost[20][20];
+int main() {
+	int n;
+	cin.tie(NULL); ios_base::sync_with_stdio(false);
+	cin >> n;
+		
+	return 0;
+}
