57b63b98f618e8ff22fa767a0dfb84012661f879
calculus0129 <ryanbhang129@naver.com>
2024-08-12 21:10:19 +0900

    try solving b1311

--- BOJ/C++/b1311.cc
+++ BOJ/C++/b1311.cc
@@ -6,6 +6,10 @@ int main() {
 	int n;
 	cin.tie(NULL); ios_base::sync_with_stdio(false);
 	cin >> n;
-		
+	for(int i=0,j;i<n;++i) for(j=0;j<n;++j) {
+		cin >> cost[i][j];
+	}		
+	
+	cout << dp[(1<<n)-1];
 	return 0;
 }
