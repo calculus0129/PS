55e973f2f229ee89eef77a256b68efa093760047
calculus0129 <ryanbhang129@naver.com>
2024-08-12 21:16:59 +0900

    try solving b1311

--- BOJ/C++/b1311.cc
+++ BOJ/C++/b1311.cc
@@ -9,7 +9,12 @@ int main() {
 	for(int i=0,j;i<n;++i) for(j=0;j<n;++j) {
 		cin >> cost[i][j];
 	}		
-	
+	for(int i=1,j,num;i<(1<<n);++i) {
+		dp[i] = BIG;
+		num = 0;
+		for(j=0;j<n;++j) if(i & (1<<j)) ++num;
+		for(j=0;j<n;++j) dp[i] = min(dp[i&~(1<<j)] + cost[num-1][j], dp[i]);
+	}	
 	cout << dp[(1<<n)-1];
 	return 0;
 }
