686ebfbd8a11a6b4a5e4c70af94d44e2857dd3ef
calculus0129 <ryanbhang129@naver.com>
2024-10-20 23:11:26 +0900

    solved b11718

---
+++ BOJ/C++/b11718.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    string s;
+    while(getline(cin, s)) {
+        cout << s << '\n';
+    }
+    return 0;
+}
