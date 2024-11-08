135b2efdbdcd56dcf1cc1d3e2aca60c8837cfa8d
calculus0129 <ryanbhang129@naver.com>
2024-11-08 17:27:25 +0900

    solved b25288

---
+++ BOJ/C++/b25288.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    int n; cin >> n;
+    string s; cin >> s;
+    while(n--) cout << s;
+    return 0;
+}
