a6098ec9568d55bc805c224d95eed318dece308f
calculus0129 <ryanbhang129@naver.com>
2024-11-14 01:05:13 +0900

    solved b10817

---
+++ BOJ/C++/b10817.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[3];
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr, arr+3);
+    cout << arr[1];
+    return 0;
+}
