10afdf11abc55aaa1c70cbddc01105682ccd4f5e
calculus0129 <ryanbhang129@naver.com>
2025-05-31 15:59:04 +0900

    solved b11719

---
+++ BOJ/C++/b11719.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    char ch;
+    while((ch = cin.get()) != EOF) {
+        cout << ch;
+    }
+    return 0;
+}
