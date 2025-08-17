06dd0d836dd72e47ac6aef9363452b7e48cb91c7
calculus0129 <ryanbhang129@naver.com>
2025-08-17 11:02:19 +0900

    update c++ compilation shell script

--- BOJ/C++/make.sh
+++ BOJ/C++/make.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+function manual()
+{
+echo "Usage: $0 <c++17 source code> [OPTION]
+
+Compiles a c++17 source code and executes when successfully compiled.
+
+If there are more than one argument, then we use address sanitizer (ASAN) during compilation.
+Only the first argument is treated as the compile target."
+}
+
 if [ $# -eq 0 ]; then
-    echo "no argument!"
+    manual
 else
-    if [ $# -gt 2 ]; then
+    echo "Compiling..."
+    if [ $# -ge 2 ]; then
+        echo "ASAN mode activated"
         g++ -Wall -g -O2 -std=c++17 $1 -o pgm -fsanitize=address
     else
         g++ -Wall -g -O2 -std=c++17 $1 -o pgm
     fi
-    ./pgm
+
+    if [ $? -eq 0 ]; then
+        echo "Compile SUCCESS!"
+        ./pgm
+    else
+        echo "Compile FAILED!"
+    fi
 fi
